9dfe408f3cf4d0e5e8af94b699e322c16344bd31
BryantFeliz <bryantf.rodriguez02@gmail.com>
2023-06-14 11:31:00 -0400

    Tester branch

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 let User = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 },{collection:"user-data"});
 
+let Comment = new mongoose.Schema({
+    text: { type: String, required: true},
+    date: { type: Date, default: Date.now},
+});
 
-module.exports = mongoose.model("User", User);
+module.exports = mongoose.model("User", User), mongoose.model("Comments", Comment);
 
 
 
